0b237bbdc9ec2ab5b35822d56131040769a975c9
Kitaro <Nordic_woodpecker@yahoo.co.jp>
2020-05-29 11:53:00 +0900

    I'm confed x(1), x(2), x1, x2.

--- rg4.f90
+++ rg4.f90
@@ -2,26 +2,33 @@ program main
     implicit none
     real,parameter ::  Ni = 0.0, Nf = -log(3001.0) ! e-folding numberの最初と最後を指定
     ! print *, Ni, Nf
-    integer n, j
-    real t, x, h, x0, f, k1, k2, k3, k4
+    integer, parameter :: n = 2
+    integer i
+    real t, x1, x2, h, f
+
+    t = 0.0
+    x1 = 0.1
+    x2 = 0.68
+    h = 0.1
+
 end program main
 
-real function f(i, t, x)
+real function f(i, t, x1, x2)
     implicit none
     integer i
-    real t, x
+    real t, x1, x2
     if (i .eq. 1) then
-        ! x(1) := \Omega_r, x(2) := \Omega_{\lambda} とする
-        f = x(1) * ( x(1) - 3.0 * x(2) - 1.0 ) ! \Omega_rに関する微分方程式
+        ! x1 := \Omega_r, x2 := \Omega_{\lambda} とする
+        f = x1 * ( x1 - 3.0 * x2 - 1.0 ) ! \Omega_rに関する微分方程式
     else if (i .eq. 2) then
-        f = x(2) * ( x(1) - 3.0 * x(2) + 3.0 ) ! \Omega_{\lambda}に関する微分方程式
+        f = x2 * ( x1 - 3.0 * x2 + 3.0 ) ! \Omega_{\lambda}に関する微分方程式
     end if
 end function
 
-subroutine runge4(t, x, n, h, f) ! Runge-Kutta methodを行うsubroutine
+subroutine runge4(t, x1, x2, n, h, f) ! Runge-Kutta methodを行うsubroutine
     implicit none 
     integer i, n
-    real t, x(i), h, f
+    real t, x1, x2, h, f
     real, allocatable, dimension(:)::k1, k2, k3, k4, work ! 任意次元の配列を用意
     allocate(k1(n), k2(n), k3(n), k4(n), work(n)) ! n次元を割り当てる
 
