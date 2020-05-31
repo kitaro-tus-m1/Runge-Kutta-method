8a91f4a2e6a6364dd53460e5135eedb016e25357
Kitaro <Nordic_woodpecker@yahoo.co.jp>
2020-05-31 19:07:40 +0900

    First output but it still needs update

--- step2/rk-step1-copy.f90
+++ step2/rk-step1-copy.f90
@@ -3,7 +3,7 @@ program main
     implicit none
 
     ! 解く方程式は x'(t) = x - 2xy =: f(x, y), y'(t) = -y + 3xy =: g(x, y)
-    ! 初期値は x(0) = 1, y(0) = 0 とする
+    ! 初期値は x(0) = 1, y(0) = 2 とする
     ! t0 := t=0, x0 := x(0), y0 := y(0)
     ! しかし配列を使って，ベクトル表現で解くためには
     ! x(2) := (x, y), f(2) := (f, g), x0(2) := (x0, y0)とおくべき
@@ -21,94 +21,62 @@ program main
 
     ! 繰り返し処理のためのiを定義する
     integer i
+    ! 函数f1とf2を区別するためのjを定義する
+    integer j
     ! Runge-Kutta method の各kを2要素配列として定義する
     real k1(2), k2(2), k3(2), k4(2)
-    ! real f
 
     ! (done) 動作確認
     ! print *, x
     ! (done) 動作確認 配列xの第2成分を表示する
     ! print *, x(2)
 
-    ! 注意点
-    ! 内部副プログラムで定義した函数を利用する際には，containsより前で呼び出さなければならない
-    ! 内部副プログラムで函数を定義した場合，
-    ! 14行目のような "real f" といった函数のデータ型指定は必要ない
-
     ! (done)動作確認
     !print *, f(10.0, 8.0)
     !print *, f(x, y)
 
     ! 初期値を画面に表示
-    print *, x
+    print *, t, x
 
     ! (done) 動作確認 函数fの初期値を画面に表示
     ! do i = 1, 2
     ! print *, f(i, t, x)
     ! end do
 
+! 'rk-step2.dat'を開く
+open(10, file='rk-step2.dat', status='replace')
 
-    ! 'rk-step1.dat'を開く
-    open(10, file='rk-step2.dat', status='replace')
-    ! 初期値を'rk-step2.dat'に出力
-    write(10, *) x
-
+! RUnge-Kutta methodの繰り返し処理を書く
 do i = 1, 100
-    k1 = h * f(x, y)
-    k2 = h * f(x+h/2.0, y+k1/2.0)
-    k3 = h * f(x+h/2.0, y+k2/2.0)
-    k4 = h* f(x+h, y+k3)
-    x = x + h
-    y = y + (k1 + 2*k2 + 2*k3 + k4)/6.0
+    ! f1, f2のそれぞれについて処理する
+    do j = 1, 2
+        k1(j) = h * f(j, t, x)
+        k2(j) = h * f(j, t+h/2.0, x+k1(j)/2.0)
+        k3(j) = h * f(j, t+h/2.0, x+k2(j)/2.0)
+        k4(j) = h * f(j, t+h, x+k3(j))
+        t = t + h
+        x(j) = x(j) + (k1(j) + 2*k2(j) + 2*k3(j) + k4(j))/6.0
+    end do
     ! 計算結果を画面に表示
-    print *, x, y
-
-    ! 計算結果を'rk-step1.dat'に出力
-        write(10, *) x, y
+    print *, t, x
+    ! 計算結果を'rk-step2.dat'に出力
+    write(10, *) t, x
 end do
-
-! 'rk-step1.dat'を閉じる
+! 'rk-step2.dat'を閉じる
 close(10)
 
-! 'rk-step1.plt'を作成してgnuplotでAqauaTermによるプロットを表示させる
-! 'rk-step1.plt'を開くgnuplotのスクリプトファイル
-open(11, file='rk-step1.plt', status='replace')
-    write (11, '(a)') 'plot "rk-step1.dat"'
-    ! Returnを押すまで待機
-    !write (11, '(a)') 'pause -1'
-! 'rk-step1.plt'を閉じる
-close(11)
-
-! gnuplotを起動し，'rk-step1.plt'を実行する
-call execute_command_line('gnuplot "rk-step1.plt"')
-
-! 'rk-step1-save.plt'を作成してgnuplotでpng画像としてプロットを保存する
-! 'rk-step1-save.plt'を開くgnuplotのスクリプトファイル
-open(12, file='rk-step1-save.plt', status='replace')
-    ! 出力先をpngに設定
-    write (12, '(a)') 'set terminal png'
-    ! 出力ファイルを'rk-step1.png'に設定
-    write (12, '(a)') 'set output "rk-step1.png"'
-    ! 'rk-step1.dat'をプロット
-    write (12, '(a)') 'plot "rk-step1.dat"'
-! 'rk-step1-save.plt'を閉じる
-close(12)
-
-! gnuplotを起動し，'rk-step1-save.plt'を実行する
-call execute_command_line('gnuplot "rk-step1-save.plt"')
-
-    stop
+stop
 
 ! 以下，内部副プログラム
 contains
-    ! 1解微分の右辺を関数f(i,t,x,y) in { f1(t,x,y), f2(t,x,y) }として定義する
-    real function f(i, t, x)
+    ! 1解微分の右辺を関数f(j,t,x(1),x(2)) in { f1(t,x(1),x(2)), f2(t,x(1),x(2)) }として定義する
+    real function f(j, t, x)
         implicit none
         real t, x(2)
-        integer i
-        if (i .eq. 1) then
+        integer j
+        if (j .eq. 1) then
             f = x(1) - 2 * x(1) * x(2)
-        else if (i .eq. 2) then
+        else if (j .eq. 2) then
             f = -x(2) + 3 * x(1) * x(2)
         end if
         return
